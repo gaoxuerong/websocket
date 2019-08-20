c219d4f62d41bf112d2dc17198191e07eda80889
gaoxuerong <gaoxuerong123@gmail.com>
2019-08-21 07:35:54 +0800

    手写websoket实现

--- websocket/app2.js
+++ websocket/app2.js
@@ -1,7 +1,45 @@
 let net = require('net')
+let crypto = require('crypto')
+const CODE = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 let server = net.createServer((function(socket){
+  socket.once('data',function(data){
+    data = data.toString()
+    console.log(data)
+    if(data.match(/Connection: Upgrade/)){
+      let rows = data.split('\r\n')
+      rows = rows.slice(1,-2)
+      let headers = {}
+      headers = rows.reduce((pre,item) => {
+        let [key, value] = item.split(': ')
+        pre[key] = value
+        return pre;
+      },headers)
+      if(headers['Sec-WebSocket-Version'] == 13) {
+        let setSecWebSocketKey = headers['Sec-WebSocket-Key']
+        let SecWebSocketAccept = crypto.createHash('sha1').update(setSecWebSocketKey+CODE).digest('base64')
+        let response = [
+          'HTTP/1.1 101 Switching Protocols',
+          'Upgrade: websocket',
+          'Connection: Upgrade',
+          `Sec-WebSocket-Accept: ${SecWebSocketAccept}`,
+          '\r\n'
+        ].join('\r\n')
+        socket.write(response)
+        socket.on('data', function() {
 
+        })
+      }
+    }
+  })
 }))
+
+server.listen(9999);
+
+
+
+
+
+
 /** 请求头
  * GET ws://localhost:8888/ HTTP/1.1
 Host: localhost:8888
