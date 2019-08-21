194f19b0a740a099a6b60fa1c0e0376e8e9486a8
gaoxuerong <gaoxuerong123@gmail.com>
2019-08-22 07:23:29 +0800

    完善手写websocket

--- websocket/app2.js
+++ websocket/app2.js
@@ -25,14 +25,32 @@ let server = net.createServer((function(socket){
           '\r\n'
         ].join('\r\n')
         socket.write(response)
-        socket.on('data', function() {
-
+        socket.on('data', function(buffers) {
+          //<Buffer 81 87 9d af 63 f7 ae 81 0b 92 f1 c3 0c>
+          let _fin = buffers[0]&0b10000000 == 0b10000000 // 结束位是false还是true
+          let _opcode = buffers[0]&0b00001111 // 操作码
+          let _isMask = buffers[1]&0b10000000 == 0b10000000 // 是否进行了掩码
+          let _PayloadLen = buffers[1]&0b01111111
+          let _mask = buffers.slice(2,6)
+          let _Payload = buffers.slice(6)
+          unmask(_Payload, _mask);
+          //向客户端响应数据
+          let response = Buffer.alloc(2 + _PayloadLen);
+          response[0] = _opcode | 0b10000000;//1表示发送结束
+          response[1] = _PayloadLen;//负载的长度
+          _Payload.copy(response, 2);
+          socket.write(response);
         })
       }
     }
   })
 }))
-
+function unmask(buffer, mask) {
+  const length = buffer.length;
+  for (let i = 0; i < length; i++) {
+      buffer[i] ^= mask[i%4];
+  }
+}
 server.listen(9999);
 
 
