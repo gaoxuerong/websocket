415a8873922767a4f3cd6c4a116e411bd5e50384
gaoxuerong <gaoxuerong123@gmail.com>
2019-08-24 07:59:07 +0800

    feat add chat-room content

--- chat-socket.io/app.js
+++ chat-socket.io/app.js
@@ -3,17 +3,31 @@ const path = require('path');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve('index.html'));
+  res.sendFile(path.resolve('index.html'));
 });
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+const SYSTEM = '系统';
 io.on('connection', function (socket) {
-    console.log('客户端已经连接');
-    socket.on('message', function (msg) {
-        console.log(msg);
-        socket.send('sever:' + msg);
-    });
+  let username;
+  socket.on('message', function (msg) {
+    if (username) {
+      let result = msg.match(/@([^ ]+) (.+)/);
+      if (result) {
+
+      } else {
+        socket.emit('message', {
+          user: username,
+          content: msg,
+          createAt: new Date()
+      });
+      }
+    } else {
+      username = msg;
+      socket[username] = socket;
+      socket.broadcast.emit('message', { user: SYSTEM, content: `${username}加入了聊天室`, createAt: new Date() });
+    }
+  });
 });
 server.listen(3000);
