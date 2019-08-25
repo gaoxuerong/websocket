3b8bf5264d6b2acb67272bf388538d5f2116cf1c
gaoxuerong <gaoxuerong123@gmail.com>
2019-08-25 19:57:52 +0800

    补全聊天室代码

--- chat-socket.io/app.js
+++ chat-socket.io/app.js
@@ -9,24 +9,41 @@ app.get('/', function (req, res) {
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const SYSTEM = '系统';
+let sockets = {}
 io.on('connection', function (socket) {
   let username;
   socket.on('message', function (msg) {
     if (username) {
       let result = msg.match(/@([^ ]+) (.+)/);
       if (result) {
-
+        let toUser = result[1]
+        let content = result[2]
+        let toSocket = socket[toUser]
+        toSocket ? toSocket.emit('message', {
+          user: username,
+          content: msg,
+          createAt: new Date()
+        }) : socket.send({
+          user: SYSTEM,
+          content: `你私聊的用户不在线`,
+          createAt: new Date()
+        });
       } else {
-        socket.emit('message', {
+        io.emit('message', {
           user: username,
           content: msg,
           createAt: new Date()
       });
       }
     } else {
-      username = msg;
-      socket[username] = socket;
-      socket.broadcast.emit('message', { user: SYSTEM, content: `${username}加入了聊天室`, createAt: new Date() });
+      let oldsoket = sockets[msg]
+        if(oldsoket){
+          socket.emit('message',{ user: SYSTEM, content: `${username} 已经被占用，换个用户名吧`, createAt: new Date() })
+        }else {
+          username = msg;
+          socket[username] = socket;
+          socket.broadcast.emit('message', { user: SYSTEM, content: `${username}加入了聊天室`, createAt: new Date() });
+        }
     }
   });
 });
